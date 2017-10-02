ef23c8750bb23a4baaf5ad8f158c97847ee65646
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-10-02 12:20:55 +0330

    feat : tprint function and text_dic with block fonts added

--- art/__init__.py
+++ art/__init__.py
@@ -1,2 +1,3 @@
 from .art import *
 from .art_dic import *
+from .text_dic import *
--- art/art.py
+++ art/art.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .art_dic import *
-
+from .text_dic import *
 def aprint(artname,number=1,text=""):
     '''
     This function print ascii art
@@ -37,4 +37,24 @@ def art(artname,number=1,text=""):
         print("[Error] Invalid Art Name")
     except Exception:
         print("[Error] Return Faild!")
+
+def tprint(text):
+    try:
+        split_list=[]
+        result_list=[]
+        for i in text.lower():
+            split_list.append(text_dic[i].split("\n"))
+        for i in range(len(split_list[0])):
+            temp=""
+            for j in range(len(split_list)):
+                if j>0 and (i==1 or i==len(split_list[0])-2):
+                    temp=temp+" "
+                temp=temp+split_list[j][i]
+            result_list.append(temp)
+        print(("\n").join(result_list))
+
+    except KeyError:
+        print("[Error] Invalid Char!")
+    except Exception:
+        print("[Error] Print Faild!")
 
