5445d1507de1c27bb5d8e70ebb080d28a0d8e16a
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-10-29 02:05:30 +0330

    migrate to version 0.3

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'art',
   packages = ['art'],
-  version = '0.2',
+  version = '0.3',
   description = 'ASCII Art Collection In Python',
   long_description='ASCII Art Collection In Python',
   author = 'Sepand Haghighi',
   author_email = 'sepand@qpage.ir',
   url = 'https://github.com/sepandhaghighi/art',
-  download_url = 'https://github.com/sepandhaghighi/art/tarball/v0.2',
+  download_url = 'https://github.com/sepandhaghighi/art/tarball/v0.3',
   keywords = ['ascii', 'art', 'python3','python','text'],
   install_requires=[
 	  'codecov',
