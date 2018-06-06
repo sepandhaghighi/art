35c578238acec659a6f9c5ca534cf5febf868b2a
sepandhaghighi <sepand.haghighi@yahoo.com>
2018-06-06 16:12:54 +0430

    fix : autopep8 fix

--- art/art.py
+++ art/art.py
@@ -70,13 +70,13 @@ font_map = {"block": [block_dic, True], "banner": [banner_dic, False],
             "binary": [binary_dic, False], "bubble": [bubble_dic, False],
             "calgphy2": [calgphy2_dic, False],
             "cygnet": [cygnet_dic, False], "diamond": [diamond_dic, False],
-            "eftifont": [eftifont_dic, False],"eftitalic":[eftitalic_dic,
-                                                           False],
-            "eftiwater":[eftiwater_dic,False],"fourtops":[fourtops_dic,False],
-            "goofy":[goofy_dic,True],"hollywood":[hollywood_dic,False],
-            "invita":[invita_dic,False],"italic":[italic_dic,False],
-            "jazmine":[jazmine_dic,False],"lcd":[lcd_dic,False],"lean":[
-        lean_dic,False]}
+            "eftifont": [eftifont_dic, False], "eftitalic": [eftitalic_dic,
+                                                             False],
+            "eftiwater": [eftiwater_dic, False], "fourtops": [fourtops_dic, False],
+            "goofy": [goofy_dic, True], "hollywood": [hollywood_dic, False],
+            "invita": [invita_dic, False], "italic": [italic_dic, False],
+            "jazmine": [jazmine_dic, False], "lcd": [lcd_dic, False], "lean": [
+    lean_dic, False]}
 
 DEFAULT_FONT = "standard"
 
@@ -300,8 +300,8 @@ def text2art(text, font=DEFAULT_FONT, chr_ignore=True):
         raise artError("font should have str type")
     font = font.lower()
     fonts = sorted(font_map.keys())
-    if font=="random":
-        random_index = random.randint(0,len(font_map.keys()))
+    if font == "random":
+        random_index = random.randint(0, len(font_map.keys()))
         font = fonts[random_index]
     elif font not in font_map.keys():
         distance_list = list(map(lambda x: distance_calc(font, x), fonts))
