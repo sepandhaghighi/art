4202e51e7c6a11de3cdda4ce33f3521259ecfd3d
sepandhaghighi <sepand.haghighi@yahoo.com>
2018-12-23 01:57:24 +0330

    fix : test some irrelevant part removed

--- art/test.py
+++ art/test.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 '''
-This function get a string as input if input is one digit add a zero
-:param input_string: input digit az string
-:type input_string:str
-:return: modified output as str
 >>> import os
 >>> import random
 >>> import sys
