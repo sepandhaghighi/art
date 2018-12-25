31b97f94499087698ea77fe0ec9986466e25334a
sepandhaghighi <sepand.haghighi@yahoo.com>
2018-12-25 23:41:59 +0330

    fix : testcases updated for new fonts

--- art/art.py
+++ art/art.py
@@ -342,7 +342,7 @@ def font_list(text="test"):
     '''
     for item in sorted(list(font_map.keys())):
         print(str(item) + " : ")
-        if str(item) in ["char4", "c2"]:
+        if str(item) in ["char4", "c2", "war_of_w"]:
             tprint(text.upper(), str(item))
         else:
             tprint(text, str(item))
