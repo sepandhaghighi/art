ccefb71fe7ccefb38116197388d263f570f00575
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-01-15 01:56:02 +0330

    fix : minor edit in travis setting

---
+++ .travis/before_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+	brew update --debug --verbose
+	brew install openssl readline
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
+    pyenv install $PYTHON
+    export PYENV_VERSION=$PYTHON
+    export PATH="/Users/travis/.pyenv/shims:${PATH}"
+    pyenv virtualenv venv
+    source venv/bin/activate
+fi
