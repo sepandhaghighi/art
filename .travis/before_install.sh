01834e735ee51baee2204b52d56597a2e65aed6e
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-01-15 15:31:42 +0330

    fix : travis updated

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -7,6 +7,6 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     pyenv install $PYTHON
     export PYENV_VERSION=$PYTHON
     export PATH="/Users/travis/.pyenv/shims:${PATH}"
-    pyenv global $PYTHON
+    pyenv local $PYTHON
 	exit 0
 fi
