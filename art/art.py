81410fbc9b1468e225e22d61175a89ba608c2633
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-01-18 01:57:45 +0330

    fix : some new # pragma: no cover added to art.py

--- art/art.py
+++ art/art.py
@@ -23,10 +23,10 @@ XLARGE_WIZARD_FONT = ["dotmatrix","sweet","hollywood","nscript","georgia11","blo
 DESCRIPTION = '''ASCII art is also known as "computer text art".
 It involves the smart placement of typed special characters or
 letters to make a visual shape that is spread over multiple lines of text.
-ART is a Python lib for text converting to ASCII art fancy.'''
+ART is a Python lib for text converting to ASCII art fancy.''' # pragma: no cover
 
 
-class artError(Exception):
+class artError(Exception): # pragma: no cover
     pass
 
 
