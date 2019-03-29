961861a957c0f59323e99ecac8df986f8a489797
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-03-29 16:57:34 +0430

    fix : test.sh fixed

--- otherfile/test.sh
+++ otherfile/test.sh
@@ -7,6 +7,6 @@ if [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
   then
       python -m vulture --min-confidence 80 --exclude=art,build,.eggs --sort-by-size .
       python -m bandit -r art -s B311
-	  python -m pydocstyle --match=(?!test).*\.py
+	  python -m pydocstyle --match='(?!test).*\.py'
   fi
 python -m cProfile -s cumtime art_profile.py
