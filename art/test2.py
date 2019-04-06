65051d063fdb13df92d733a73b4136cdcadf9ff3
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-04-06 20:19:04 +0430

    fix : test2.py updated

--- art/test2.py
+++ art/test2.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
+>>> import os
 >>> from art import *
 >>> from art.art_param import TEST_FILTERED_FONTS
 >>> for i in sorted(TEST_FILTERED_FONTS):
@@ -7,5 +8,16 @@
 ϝԍƨϝ
 ɈƨǝɈ
 ʇsǝʇ
+>>> for i in sorted(TEST_FILTERED_FONTS):
+...	    Data = tsave("test@34",font=i,filename=i)
+Saved!
+Filename: flip.txt
+Saved!
+Filename: mirror.txt
+Saved!
+Filename: mirror_flip.txt
+>>> os.remove("flip.txt")
+>>> os.remove("mirror.txt")
+>>> os.remove("mirror_flip.txt")
 
 '''
