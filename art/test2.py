ae17f345829a31bf0813fe95ed3336f677e157fb
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-04-11 14:53:14 +0430

    fix : testcases updated

--- art/test2.py
+++ art/test2.py
@@ -6,8 +6,13 @@
 >>> for i in sorted(TEST_FILTERED_FONTS):
 ...	    tprint("test",font=i)
 ϝԍƨϝ
+<BLANKLINE>
 ɈƨǝɈ
+<BLANKLINE>
 ʇsǝʇ
+<BLANKLINE>
+ⓣⓔⓢⓣ
+<BLANKLINE>
 >>> for i in sorted(TEST_FILTERED_FONTS):
 ...	    Data = tsave("test@34",font=i,filename=i)
 Saved!
@@ -16,8 +21,11 @@ Saved!
 Filename: mirror.txt
 Saved!
 Filename: mirror_flip.txt
+Saved!
+Filename: white_bubble.txt
 >>> os.remove("flip.txt")
 >>> os.remove("mirror.txt")
 >>> os.remove("mirror_flip.txt")
+>>> os.remove("white_bubble.txt")
 
 '''
