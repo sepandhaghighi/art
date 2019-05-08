a345da2058cb86b8a64bbff808d532cc5c9f1bcf
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-05-09 01:54:00 +0430

    font : fancy21 font added

--- art/art_param.py
+++ art/art_param.py
@@ -186,7 +186,8 @@ RANDOM_FILTERED_FONTS = [
     "fancy17",
     "fancy18",
     "fancy19",
-    "fancy20"]
+    "fancy20",
+    "fancy21"]
 
 RANDOM_FILTERED_ARTS = [
     "message2",
@@ -234,7 +235,8 @@ TEST_FILTERED_FONTS = [
     "fancy17",
     "fancy18",
     "fancy19",
-    "fancy20"]
+    "fancy20",
+    "fancy21"]
 
 DESCRIPTION = '''ASCII art is also known as "computer text art".
 It involves the smart placement of typed special characters or
@@ -601,7 +603,8 @@ FONT_MAP = {"block": [block_dic, True], "banner": [banner_dic, False],  # pragma
             "fancy17": [fancy17_dic, False],
             "fancy18": [fancy18_dic, False],
             "fancy19": [fancy19_dic, False],
-            "fancy20": [fancy20_dic, False]
+            "fancy20": [fancy20_dic, False],
+            "fancy21": [fancy21_dic, False]
             }
 
 FONT_COUNTER = len(FONT_MAP)  # pragma: no cover
