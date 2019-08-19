8fb26f723146d1fd30b5e8ca4ebd16bfde2fe255
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-08-19 12:08:05 +0430

    fix : autopep8.bat modified

--- autopep8.bat
+++ autopep8.bat
@@ -1,3 +1,4 @@
 python -m autopep8 art --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
 python -m autopep8 setup.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
+python -m autopep8 art_profile.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
 python -m autopep8 otherfile --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
