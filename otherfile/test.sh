967c53de77cfca6b2f7f153d9fc70e106c2a10e5
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-08-22 00:25:17 +0430

    fix : minor edit in test.sh

--- otherfile/test.sh
+++ otherfile/test.sh
@@ -17,9 +17,9 @@ if [ "$IS_IN_TRAVIS" = 'false' ] || [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
       python -m vulture --min-confidence 80 --exclude=art,build,.eggs --sort-by-size .
       python -m bandit -r art -s B311
 	  python -m pydocstyle --match='(?!test).*\.py'
-	  python -m art testcov2
-	  codecov
   else
 	  python -m art testcov
   fi
+python -m art testcov2
+codecov
 python -m cProfile -s cumtime art_profile.py
