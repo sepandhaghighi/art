476bb87da0b88d801239accc46b0a27ef447a00f
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-08-23 01:52:18 +0430

    fix : install.sh added

---
+++ .travis/install.sh
@@ -0,0 +1,23 @@
+set -e
+set -x
+
+
+PYTHON_COMMAND=python
+PIP_COMMAND=pip
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]
+then
+	 PYTHON_COMMAND=python3
+	 PIP_COMMAND=pip3
+fi
+ 
+$PIP_COMMAND install -r requirements.txt
+$PYTHON_COMMAND setup.py install
+$PYTHON_COMMAND -m art test
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]
+then
+	$PIP_COMMAND install --upgrade --upgrade-strategy=only-if-needed -r dev-requirements.txt --user
+else
+	$PIP_COMMAND install --upgrade --upgrade-strategy=only-if-needed -r dev-requirements.txt
+fi
