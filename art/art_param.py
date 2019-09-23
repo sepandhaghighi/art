c503c029101b26e8a6d406490f21d779797e9eaf
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-09-23 16:33:25 +0330

    font : paranormal font added #69

--- art/art_param.py
+++ art/art_param.py
@@ -258,7 +258,8 @@ RANDOM_FILTERED_FONTS = [
     "fancy85",
     "fancy86",
     "white_square",
-    "black_square"]
+    "black_square",
+    "paranormal"]
 
 RANDOM_FILTERED_ARTS = [
     "message2",
@@ -378,7 +379,8 @@ TEST_FILTERED_FONTS = [
     "fancy85",
     "fancy86",
     "white_square",
-    "black_square"]
+    "black_square",
+    "paranormal"]
 
 DESCRIPTION = '''ASCII art is also known as "computer text art".
 It involves the smart placement of typed special characters or
@@ -811,7 +813,8 @@ FONT_MAP = {"block": [block_dic, True], "banner": [banner_dic, False],  # pragma
             "fancy85": [fancy85_dic, False],
             "fancy86": [fancy86_dic, False],
             "white_square": [white_square_dic, False],
-            "black_square": [black_square_dic, False]
+            "black_square": [black_square_dic, False],
+            "paranormal": [paranormal_dic, False]
             }
 
 FONT_COUNTER = len(FONT_MAP)  # pragma: no cover
