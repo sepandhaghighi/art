055012d46b43f83ada7eaf3c15042365f93f9afc
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-09-23 14:20:29 +0330

    font : white_square font added

--- art/art_param.py
+++ art/art_param.py
@@ -256,7 +256,8 @@ RANDOM_FILTERED_FONTS = [
     "fancy83",
     "fancy84",
     "fancy85",
-    "fancy86"]
+    "fancy86",
+    "white_square"]
 
 RANDOM_FILTERED_ARTS = [
     "message2",
@@ -374,7 +375,8 @@ TEST_FILTERED_FONTS = [
     "fancy83",
     "fancy84",
     "fancy85",
-    "fancy86"]
+    "fancy86",
+    "white_square"]
 
 DESCRIPTION = '''ASCII art is also known as "computer text art".
 It involves the smart placement of typed special characters or
@@ -805,7 +807,8 @@ FONT_MAP = {"block": [block_dic, True], "banner": [banner_dic, False],  # pragma
             "contouring4": [contouring4_dic, False],
             "love2": [love2_dic, False],
             "fancy85": [fancy85_dic, False],
-            "fancy86": [fancy86_dic, False]
+            "fancy86": [fancy86_dic, False],
+            "white_square": [white_square_dic, False]
             }
 
 FONT_COUNTER = len(FONT_MAP)  # pragma: no cover
