0142402f104a28c23092d5abe83b8def27841740
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-12-08 18:55:04 +0330

    feat : Example2.m added #82

---
+++ MATLAB/Example2.m
@@ -0,0 +1,39 @@
+close all
+
+try
+    python_version = pyversion;
+    fprintf(2,'** Python Version : %s\n',python_version);
+catch e
+    fprintf(2,'** Error : %s\n',e.message);
+end
+
+% Import art lib (version > 4.2)
+artlib = py.importlib.import_module('art');
+
+% art function
+% art(artname, number=1, text="")
+art1 = artlib.art('coffee');
+disp(char(art1));
+
+art2 = artlib.art('love_you',int32(1),'test');
+disp(char(art2));
+
+art3 = artlib.art('random');
+disp(char(art3));
+
+art4 = artlib.art('rand');
+disp(char(art4));
+
+% aprint function
+% aprint(artname, number=1, text="")
+artlib.aprint('coffee');
+
+artlib.aprint('love_you',int32(1),'test');
+
+artlib.aprint('random');
+
+artlib.aprint('rand');
+
+% randart function
+% randart()
+artlib.randart();
