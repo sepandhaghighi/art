6db70eaa307f0e98307e29e82add1ecdaa5957f0
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-07-25 17:24:33 +0430

    fix : font_check.py updated

--- otherfile/font_check.py
+++ otherfile/font_check.py
@@ -2,6 +2,7 @@
 """Fonts height, duplication and UTF-8 compatibility testing script."""
 import sys
 import art
+from art.art_param import NON_ASCII_FONTS
 
 Failed1 = 0
 Failed2 = 0
@@ -29,6 +30,19 @@ def is_utf8(s):
         return True
     except Exception:
         return False
+
+def is_ascii(s):
+    """
+    Check input string for ASCII compatibility.
+
+    :param s: input string
+    :type s: str
+    :return: result as bool
+    """
+    for i in s:
+        if ord(i) > 127:
+            return False
+    return True
 
 
 def print_result(flag_list, message_list):
@@ -58,12 +72,17 @@ if __name__ == "__main__":
                 s.append(
                     len(art.get_font_dic(font)[letter].split("\n")))
             l += art.get_font_dic(font)[letter]
+        ascii_flag = is_ascii(l)
         if len(set(s)) != 1:
             print("Height error in font : " + font)
             Failed1 += 1
         if not is_utf8(l):
             Failed3 += 1
             print("UTF-8 compatibility error in font : " + font)
+        if ascii_flag and font in NON_ASCII_FONTS:
+            print("Font type warning : {0} is ASCII but imported as NON-ASCII".format(font))
+        if not ascii_flag and font not in NON_ASCII_FONTS:
+            print("Font type warning : {0} is NON-ASCII but imported as ASCII".format(font))
 
     for font1 in Font_List:
         for font2 in Font_List:
