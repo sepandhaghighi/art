688d0104dde8aafbd8fc6ce8c5f2a6d1468e484b
sepandhaghighi <sepand.haghighi@yahoo.com>
2022-03-23 01:12:50 +0430

    fix : notebook_check script added

---
+++ otherfile/notebook_check.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Notebook-check script."""
+import os
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from art import tprint
+
+NOTEBOOKS_LIST = [
+    "ArtList",
+    "DecorList",
+    "FontList"]
+
+EXTENSION = ".ipynb"
+
+if __name__ == "__main__":
+    tprint("ART", "bulbhead")
+    print("Processing ...")
+    for index, notebook in enumerate(NOTEBOOKS_LIST):
+        ep = ExecutePreprocessor(timeout=6000, kernel_name='python3')
+        path = os.path.join("/", notebook)
+        with open(path + EXTENSION, "r", encoding="utf-8") as f:
+            nb = nbformat.read(f, as_version=4)
+            ep.preprocess(nb, {'metadata': {'path': '/'}})
+        with open(path + EXTENSION, 'w', encoding='utf-8') as f:
+            nbformat.write(nb, f)
+        print("{0}.{1} [OK]".format(str(index + 1), notebook))
