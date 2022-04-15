61a66e3876ac84c59d71bd7dc66cac01b549c471
sepandhaghighi <sepand.haghighi@yahoo.com>
2022-04-15 15:49:31 +0430

    fix : autopep8

--- art/art_param.py
+++ art/art_param.py
@@ -909,7 +909,16 @@ RANDOM_FILTERED_FONTS = [
     'moscow',
     'dwhistled'] + NON_ASCII_FONTS
 
-MIX_FILTERED_FONTS = ["tarty1", "tarty2", "tarty3", "tarty4", "tarty5", "tarty6", "tarty7", "tarty8", "tarty9"]
+MIX_FILTERED_FONTS = [
+    "tarty1",
+    "tarty2",
+    "tarty3",
+    "tarty4",
+    "tarty5",
+    "tarty6",
+    "tarty7",
+    "tarty8",
+    "tarty9"]
 
 DESCRIPTION = '''ASCII art is also known as "computer text art".
 It involves the smart placement of typed special characters or
@@ -1487,10 +1496,10 @@ FONT_MAP = {"block": [block_dic, True], "banner": [banner_dic, False],  # pragma
             "tarty3": [tarty3_dic, False],
             "tarty4": [tarty4_dic, False],
             "tarty5": [tarty5_dic, False],
-            "tarty6":[tarty6_dic,False],
-            "tarty7":[tarty7_dic,False],
-            "tarty8":[tarty8_dic,False],
-            "tarty9":[tarty9_dic,False]
+            "tarty6": [tarty6_dic, False],
+            "tarty7": [tarty7_dic, False],
+            "tarty8": [tarty8_dic, False],
+            "tarty9": [tarty9_dic, False]
             }
 
 DECORATIONS_MAP = {"angry1": angry1,  # pragma: no cover
