933385a0b4b3540b712e86d5ca6526f448cd86ba
sepandhaghighi <sepand.haghighi@yahoo.com>
2022-11-11 13:46:06 +0330

    font : stylish font added

--- art/art_param.py
+++ art/art_param.py
@@ -899,7 +899,8 @@ NON_ASCII_FONTS = [
     "vip",
     "crazy",
     "cruse",
-    "drippy"]
+    "drippy",
+    "stylish"]
 
 RANDOM_FILTERED_FONTS = [
     '5x8',
@@ -1521,7 +1522,8 @@ FONT_MAP = {"block": [block_dic, True], "banner": [banner_dic, False],  # pragma
             "vip":[vip_dic,False],
             "crazy":[crazy_dic,False],
             "cruse":[cruse_dic,False],
-            "drippy":[drippy_dic,False]
+            "drippy":[drippy_dic,False],
+            "stylish":[stylish_dic,False]
             }
 
 DECORATIONS_MAP = {"angry1": angry1,  # pragma: no cover
