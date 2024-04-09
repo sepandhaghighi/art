b859577312d6451b9207bb2eccb280c65b1b3657
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-04-09 12:16:41 +0430

    fix : autopep8 scripts updated

--- autopep8.sh
+++ autopep8.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python -m autopep8 art --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
-python -m autopep8 setup.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
-python -m autopep8 art_profile.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
-python -m autopep8 otherfile --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
+python -m autopep8 art --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --max-line-length 120 --verbose
+python -m autopep8 setup.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --max-line-length 120 --verbose
+python -m autopep8 art_profile.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --max-line-length 120 --verbose
+python -m autopep8 otherfile --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --max-line-length 120 --verbose
