723310700d6872ef35cdb3705c9453ce33965b77
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-07-08 18:02:01 +0430

    font : kesia added

--- art/art_param.py
+++ art/art_param.py
@@ -945,7 +945,8 @@ NON_ASCII_FONTS = [
     "lolie",
     "zakia",
     "batman",
-    "lord_of_the_ring"
+    "lord_of_the_ring",
+    "kesia"
 ]
 
 RANDOM_FILTERED_FONTS = [
@@ -1591,7 +1592,8 @@ FONT_MAP = {"block": [block_dic, True], "banner": [banner_dic, False],  # pragma
             "lolie": [lolie_dic, False],
             "zakia": [zakia_dic, False],
             "batman": [batman_dic, False],
-            "lord_of_the_ring": [lord_of_the_ring_dic, False]
+            "lord_of_the_ring": [lord_of_the_ring_dic, False],
+            "kesia": [kesia_dic, False]
             }
 
 DECORATIONS_MAP = {"angry1": angry1,  # pragma: no cover
