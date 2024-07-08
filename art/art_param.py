ece47da7ccc54751d6ee20649226a121d567bcb2
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-07-08 18:08:20 +0430

    font : awa added

--- art/art_param.py
+++ art/art_param.py
@@ -950,7 +950,8 @@ NON_ASCII_FONTS = [
     "akila",
     "laurine",
     "bianca",
-    "callian"
+    "callian",
+    "awa"
 ]
 
 RANDOM_FILTERED_FONTS = [
@@ -1601,7 +1602,8 @@ FONT_MAP = {"block": [block_dic, True], "banner": [banner_dic, False],  # pragma
             "akila": [akila_dic, False],
             "laurine": [laurine_dic, False],
             "bianca": [bianca_dic, False],
-            "callian": [callian_dic, False]
+            "callian": [callian_dic, False],
+            "awa": [awa_dic, False]
             }
 
 DECORATIONS_MAP = {"angry1": angry1,  # pragma: no cover
