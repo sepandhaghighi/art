615d3d648d2580225578c162e03c68a410bafc04
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-09-23 19:28:44 +0330

    font : neva added

--- art/params.py
+++ art/params.py
@@ -951,7 +951,8 @@ NON_ASCII_FONTS = [
     "laurine",
     "bianca",
     "callian",
-    "awa"
+    "awa",
+    "neva"
 ]
 
 RANDOM_FILTERED_FONTS = [
@@ -1603,7 +1604,8 @@ FONT_MAP = {"block": [block_dic, True], "banner": [banner_dic, False],  # pragma
             "laurine": [laurine_dic, False],
             "bianca": [bianca_dic, False],
             "callian": [callian_dic, False],
-            "awa": [awa_dic, False]
+            "awa": [awa_dic, False],
+            "neva": [neva_dic, False]
             }
 
 DECORATIONS_MAP = {"angry1": angry1,  # pragma: no cover
