4acdbc8ebf0e1082c1114c19dcad6487746a7d01
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-09-23 19:34:01 +0330

    font : abby added

--- art/params.py
+++ art/params.py
@@ -955,7 +955,8 @@ NON_ASCII_FONTS = [
     "neva",
     "smila",
     "clay",
-    "orinda"
+    "orinda",
+    "abby"
 ]
 
 RANDOM_FILTERED_FONTS = [
@@ -1611,7 +1612,8 @@ FONT_MAP = {"block": [block_dic, True], "banner": [banner_dic, False],  # pragma
             "neva": [neva_dic, False],
             "smila": [smila_dic, False],
             "clay": [clay_dic, False],
-            "orinda": [orinda_dic, False]
+            "orinda": [orinda_dic, False],
+            "abby": [abby_dic, False]
             }
 
 DECORATIONS_MAP = {"angry1": angry1,  # pragma: no cover
