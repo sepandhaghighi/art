29a6be76bcc39df55ecd6f198696d2914db929b3
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-09-23 19:31:08 +0330

    font : clay added

--- art/params.py
+++ art/params.py
@@ -953,7 +953,8 @@ NON_ASCII_FONTS = [
     "callian",
     "awa",
     "neva",
-    "smila"
+    "smila",
+    "clay"
 ]
 
 RANDOM_FILTERED_FONTS = [
@@ -1607,7 +1608,8 @@ FONT_MAP = {"block": [block_dic, True], "banner": [banner_dic, False],  # pragma
             "callian": [callian_dic, False],
             "awa": [awa_dic, False],
             "neva": [neva_dic, False],
-            "smila": [smila_dic, False]
+            "smila": [smila_dic, False],
+            "clay": [clay_dic, False]
             }
 
 DECORATIONS_MAP = {"angry1": angry1,  # pragma: no cover
