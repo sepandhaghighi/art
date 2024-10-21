016e26d0156702f24bb35fd46e06f4025d1734f0
AHReccese <amirhssin6rst@gmail.com>
2024-10-20 22:31:56 -0400

    error messages updated

--- art/params.py
+++ art/params.py
@@ -33,10 +33,10 @@ FONT_ENVIRONMENT_WARNING = "[Warning] '{0}' font is not printable in this enviro
 FONT_OR_DECOR_ENVIRONMENT_WARNING = "[Warning] '{0}' font or '{1}' decoration is not printable in this environment."
 PACKAGE_LOAD_WARNING = "[Warning] There is a problem loading the package 'coverage'."
 HEIGHT_TYPE_ERROR = "The 'height' type must be int."
-HEIGHT_RANGE_ERROR = "The `height` parameter should be >= 1."
+HEIGHT_RANGE_ERROR = "The 'height' parameter should be >= 1."
 LENGTH_TYPE_ERROR = "The 'length' type must be int."
-LENGTH_RANGE_ERROR = "The `length` parameter should be >= 1."
-CHAR_TYPE_ERROR = "The 'char' type must be str and has length of 1."
+LENGTH_RANGE_ERROR = "The 'length' parameter should be >= 1."
+CHAR_TYPE_ERROR = "The 'char' type must be str."
 
 CLI_HELP = """
 Webpage : https://www.ascii-art.site
