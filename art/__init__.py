be992cc825d25f24dc673ea168a9afc5c67b8467
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-02-23 18:21:18 +0330

    fix : autopep8

--- art/__init__.py
+++ art/__init__.py
@@ -12,8 +12,32 @@ from .params import NON_ASCII_ARTS, NON_ASCII_FONTS, ASCII_FONTS, ASCII_ARTS
 
 __version__ = ART_VERSION
 
-__all__ = ["artError", "aprint", "art", "randart", "tprint", "tsave", "text2art", "lprint",
- "line", "decor", "help_func", "get_font_dic", "set_default", "art_list", "font_list",
- "decor_list", "ART_VERSION", "FONT_NAMES", "ART_NAMES", "DECORATION_NAMES",
- "DEFAULT_FONT", "ART_COUNTER", "FONT_COUNTER", "DECORATION_COUNTER", "NON_ASCII_ARTS", "NON_ASCII_FONTS", "ASCII_FONTS", "ASCII_ARTS"
-]
+__all__ = [
+    "artError",
+    "aprint",
+    "art",
+    "randart",
+    "tprint",
+    "tsave",
+    "text2art",
+    "lprint",
+    "line",
+    "decor",
+    "help_func",
+    "get_font_dic",
+    "set_default",
+    "art_list",
+    "font_list",
+    "decor_list",
+    "ART_VERSION",
+    "FONT_NAMES",
+    "ART_NAMES",
+    "DECORATION_NAMES",
+    "DEFAULT_FONT",
+    "ART_COUNTER",
+    "FONT_COUNTER",
+    "DECORATION_COUNTER",
+    "NON_ASCII_ARTS",
+    "NON_ASCII_FONTS",
+    "ASCII_FONTS",
+    "ASCII_ARTS"]
