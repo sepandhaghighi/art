05c6f62646aaf838b9aa684d206aed2aabe16f69
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-02-24 11:00:28 +0330

    Refactor (#306)
    * fix : __init__.py updated
    
    * fix : autopep8
    
    * fix : autopep8 scripts updated
    
    * fix : minor edit in __init__.py
    
    * fix : minor edit in autopep8.bat

--- art/__init__.py
+++ art/__init__.py
@@ -9,4 +9,34 @@ from .functions import get_font_dic, set_default, art_list, font_list, decor_lis
 from .params import ART_VERSION, FONT_NAMES, ART_NAMES, DECORATION_NAMES, DEFAULT_FONT
 from .params import ART_COUNTER, FONT_COUNTER, DECORATION_COUNTER
 from .params import NON_ASCII_ARTS, NON_ASCII_FONTS, ASCII_FONTS, ASCII_ARTS
+
 __version__ = ART_VERSION
+
+__all__ = [
+    "artError",
+    "aprint",
+    "art",
+    "randart",
+    "tprint",
+    "tsave",
+    "text2art",
+    "lprint",
+    "line",
+    "decor",
+    "help_func",
+    "get_font_dic",
+    "set_default",
+    "art_list",
+    "font_list",
+    "decor_list",
+    "FONT_NAMES",
+    "ART_NAMES",
+    "DECORATION_NAMES",
+    "DEFAULT_FONT",
+    "ART_COUNTER",
+    "FONT_COUNTER",
+    "DECORATION_COUNTER",
+    "NON_ASCII_ARTS",
+    "NON_ASCII_FONTS",
+    "ASCII_FONTS",
+    "ASCII_ARTS"]
